b6ea5e3354e033d0e24b883bee81326fbfd30006
Long Nguyen <nguyenkimlog59@gmail.com>
2022-03-03 16:03:31 +0700

    Finish Exercise 2 Task 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default class Home extends React.Component {
     this.state = {
       bootcamp: "Rookies",
       time: new Date().toLocaleTimeString(),
+      clickedTime: 0,
     };
   }
   componentDidMount() {
@@ -12,11 +13,33 @@ export default class Home extends React.Component {
       this.setState({ time: new Date().toLocaleTimeString() });
     }, 1000);
   }
+  showAlert() {
+    if (this.state.clickedTime <= 0) return;
+
+    if (this.state.clickedTime % 15 === 0) {
+      alert("FizzBuzz");
+    } else if (this.state.clickedTime % 5 === 0) {
+      alert("Buzz");
+    } else if (this.state.clickedTime % 3 === 0) {
+      alert("Fizz");
+    }
+  }
+  clickGame() {
+    this.setState(
+      {
+        clickedTime: this.state.clickedTime + 1,
+      },
+      this.showAlert
+    );
+  }
   render() {
     return (
-      <h2>
-        Welcome to {this.props.bootcamp}. It is {this.state.time}
-      </h2>
+      <React.Fragment>
+        <h2>
+          Welcome to {this.props.bootcamp}. It is {this.state.time}
+        </h2>
+        <button onClick={() => this.clickGame()}> Clicky Game</button>
+      </React.Fragment>
     );
   }
 }
