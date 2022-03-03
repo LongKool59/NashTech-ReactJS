1517b7c653d77aa08da402809b5331bc93b9ea08
Long Nguyen <nguyenkimlog59@gmail.com>
2022-03-03 14:27:20 +0700

    Finish Exercise 2 task 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
 export default class Home extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { bootcamp: "Rookies" };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      bootcamp: "Rookies",
+      time: new Date().toLocaleTimeString(),
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
   render() {
-    return <h2>Welcome to {this.props.bootcamp}</h2>;
+    return (
+      <h2>
+        Welcome to {this.props.bootcamp}. It is {this.state.time}
+      </h2>
+    );
   }
 }
